3d817b4797ab4ef979a2d1306011a0a8dd2fa10a
YounQsheyex <younqsheyex@gmail.com>
2025-09-08 05:02:17 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const fileupload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
+const userRoutes = require("./routes/userRoutes");
+const eventRoutes = require("./routes/eventRoutes");
+const PORT = process.env.PORT || 5500;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(
+  fileupload({
+    useTempFiles: true,
+    limits: { fileSize: 10 * 1024 * 1024 },
+  })
+);
+
+// ROUTES
+app.use("/", (req, res) => {
+  res.status(200).json({ success: true, message: "Welcome to Events Server" });
+});
+
+app.use("/api/auth", userRoutes);
+app.use("/api/event", eventRoutes);
+
+// error routes
+app.use("/", (req, res) => {
+  res.status(404).json({ success: false, message: "ROUTE NOT FOUND" });
+});
+
+const startServer = async (req, res) => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, { dbName: "EVENTS-DB" });
+    app.listen(PORT, () => {
+      console.log(`App Running on PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+startServer();
