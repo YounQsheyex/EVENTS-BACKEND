c44b3fb1665e9250778f6587b27877551454dd8b
YounQsheyex <younqsheyex@gmail.com>
2025-09-25 04:19:13 -0700

    feat:removed gmail sender and used sendGrid

--- emails/googleSendMail.js
+++ emails/googleSendMail.js
@@ -1,36 +1,36 @@
 const nodemailer = require("nodemailer");
-const { createWelcomeEmail } = require("./googlemailtemplate");
+const { createWelcomeEmail } = require("./googleMailTemplate");
 
 const sendGoogleWelcomeEmail = async ({ firstname, email }) => {
-    // Configure mail transporter
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD,
-        },
+  // Configure mail transporter
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  try {
+    const subject = "Welcome to Eventra!!";
+
+    // Generate the email template for Google signup
+    const html = createWelcomeEmail(firstname, process.env.FRONTEND_URL);
+
+    // Send the email
+    const info = await transporter.sendMail({
+      from: `"Eventra" <${process.env.EMAIL}>`,
+      to: email,
+      subject,
+      html,
     });
 
-    try {
-        const subject = "Welcome to Eventra!!";
-
-        // Generate the email template for Google signup
-        const html = createWelcomeEmail(firstname, process.env.FRONTEND_URL);
-
-        // Send the email
-        const info = await transporter.sendMail({
-            from: `"Eventra" <${process.env.EMAIL}>`,
-            to: email,
-            subject,
-            html,
-        });
-
-        console.log(`Google welcome email sent to ${email}: ${info.response}`);
-    } catch (error) {
-        console.error("Error sending Google welcome email:", error);
-    }
+    console.log(`Google welcome email sent to ${email}: ${info.response}`);
+  } catch (error) {
+    console.error("Error sending Google welcome email:", error);
+  }
 };
 
 module.exports = {
-    sendGoogleWelcomeEmail,
+  sendGoogleWelcomeEmail,
 };
